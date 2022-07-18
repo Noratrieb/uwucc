a0e33835ed343e319bf2a5eb46c8095ab0776ab9
nils <48135649+Nilstrieb@users.noreply.github.com>
2022-07-18 16:17:57 +0200

    add file resolver

--- parser/src/pre/mod.rs
+++ parser/src/pre/mod.rs
@@ -1,12 +1,32 @@
 mod lexer;
 
+use std::{io, path::Path};
+
 use lexer::PLexer;
 pub use lexer::{PToken, Punctuator};
 
 use crate::Span;
 
+enum IncludeKind {
+    AngleBracketed,
+    Quoted,
+}
+
+trait FileResolver {
+    fn resolve_file(&self, file_name: &Path, kind: IncludeKind) -> io::Result<Vec<u8>>;
+}
+
+struct Todo;
+
+impl FileResolver for Todo {
+    fn resolve_file(&self, file_name: &Path, kind: IncludeKind) -> io::Result<Vec<u8>> {
+        todo!()
+    }
+}
+
 pub struct Preprocessor<L> {
     lexer: L,
+    resolver: Box<dyn FileResolver>,
 }
 
 impl<'src, L> Iterator for Preprocessor<L>
@@ -23,7 +43,10 @@ where
 pub fn preprocess_tokens(src: &str) -> impl Iterator<Item = (PToken<'_>, Span)> {
     let lexer = PLexer::new(src);
 
-    let preprocessor = Preprocessor { lexer };
+    let preprocessor = Preprocessor {
+        lexer,
+        resolver: Box::new(Todo),
+    };
 
     preprocessor
 }
